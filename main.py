04b8c69649fb7f3c259915a1d00967fcd275dfe0
Gustavo Sene <gustavodev6331@gmail.com>
2026-07-04 21:25:47 -0400

    Deleted @upload route

--- main.py
+++ main.py
@@ -112,10 +112,6 @@ def download():
     as_attachment=True
 )
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    if request.method == 'POST':
-        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
